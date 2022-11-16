6b05c349bfc89a2753561a07e41a2c1b33e21066
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2022-11-17 00:21:18 +0600

    rady to server

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+// mongodb uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3pbwuyl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) =>{
+    res.send('hellow from mangrove_server');
+});
+
+app.listen(port, () =>{
+    console.log(`mangrove_server app listing or port${port}`);
+});
