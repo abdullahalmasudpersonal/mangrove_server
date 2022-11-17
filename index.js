0faf3a3620b7732fe785812716383d00cbd29133
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2022-11-17 23:09:02 +0600

    add post&get api for product&order

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -15,30 +15,80 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3pbwuyl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+async function run() {
+    try {
         await client.connect();
         const productsCollection = client.db('mangrove').collection('products');
+        const orderCollection = client.db('mangrove').collection('order');
 
+        /////////////////products///////////////
         // get product
-        app.get('/products', async(req, res) =>{
+        app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productsCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
-        })
+        });
+
+        // get product details
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        });
+
+        //post product api
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            res.send(result);
+        });
+
+        /////////////ordr//////////////////
+        // post order api
+        app.post('/order', async (req, res) => {
+            const newOrder = req.body;
+            const result = await orderCollection.insertOne(newOrder);
+            res.send(result);
+        });
+
+        // get myorder api
+        app.get('/myorder', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const myorder = await orderCollection.find(query).toArray();
+            res.send(myorder);
+        });
+
+
 
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('hellow from mangrove_server');
 });
 
-app.listen(port, () =>{
-    console.log(`mangrove_server app listing or port${port}`);
+app.listen(port, () => {
+    console.log(`mangrove_server app listing or port ${port}`);
 });
